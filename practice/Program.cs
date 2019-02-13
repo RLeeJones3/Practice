e185f58a9c6f9cb695676e821696b33bafffbe1d
Rjones <rjonessoft@gmail.com>
2019-02-12 19:20:46 -0600

    Implemented Basic Login

--- practice/Program.cs
+++ practice/Program.cs
@@ -6,7 +6,23 @@ namespace practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int password = 123;
+            string userID = "BDowds";
+
+            Console.WriteLine("Please enter your user ID:");
+            string userIdInput = Console.ReadLine();
+
+            Console.WriteLine("Please enter your password:");
+            int passwordInput = int.Parse(Console.ReadLine());
+
+            if (userIdInput == userID && passwordInput == password)
+            {
+                Console.WriteLine("Logging in...");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect ID or Pin!");
+            }
         }
     }
-}
+} 
